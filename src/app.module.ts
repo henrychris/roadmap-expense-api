6ae831baa1e9ad8276cd8f57e8480bbd95a74084
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-01 15:29:41 +0100

    create user & expense resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ExpensesModule } from './expenses/expenses.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ExpensesModule],
   controllers: [AppController],
   providers: [AppService],
 })
