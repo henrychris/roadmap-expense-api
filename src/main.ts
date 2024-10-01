37e73ce7399d72ce29529efe93dc268b90c17132
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-01 17:31:11 +0100

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as logger from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const PORT = 3000;
+
+  const app = await NestFactory.create(AppModule);
+  app.use(logger('dev'));
+
   app
     .listen(PORT)
     .then(() => {
