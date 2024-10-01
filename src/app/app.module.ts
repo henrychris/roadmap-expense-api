ce074f504cc7f89ecba6fbbb4d26b1d16b8b831e
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-01 15:32:16 +0100

    move app resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ExpensesModule } from './expenses/expenses.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from '../users/users.module';
+import { ExpensesModule } from '../expenses/expenses.module';
 
 @Module({
   imports: [UsersModule, ExpensesModule],
