3dc52007c360fb5ebaa9174a4184526959581bb9
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-02 14:18:44 +0100

    configure typeorm module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
 import { ExpensesModule } from '../expenses/expenses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
+import DatabaseConfig from 'src/config/databaseConfig';
 
 @Module({
   imports: [
@@ -17,7 +19,10 @@ import * as Joi from 'joi';
           .valid('development', 'production', 'test', 'provision')
           .default('development'),
         PORT: Joi.number().required(),
-        DB_URL: Joi.string().uri().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_USERNAME: Joi.string().required(),
         DATABASE_PASSWORD: Joi.string().required(),
       }),
       validationOptions: {
@@ -25,6 +30,26 @@ import * as Joi from 'joi';
         abortEarly: false,
       },
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<DatabaseConfig>) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DATABASE_HOST'),
+        port: configService.getOrThrow('DATABASE_PORT'),
+        username: configService.getOrThrow('DATABASE_USERNAME'),
+        password: configService.getOrThrow('DATABASE_PASSWORD'),
+        database: configService.getOrThrow('DATABASE_NAME'),
+        autoLoadEntities: true,
+        migrations: [__dirname + 'data/migrations/**/*{.ts,.js}'],
+        cli: {
+          migrationsDir: __dirname + 'data/migrations/',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    // next: create db entities, create migration, use test endpoint to create a test user, note down what i did.
+    // installed packages
+    // added new config
+    // added typeOrm module
   ],
   controllers: [AppController],
   providers: [AppService],
