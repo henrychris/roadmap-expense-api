ced786640c4f29ae77fe3394b01d8ff93c15d2e9
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-02 14:19:16 +0100

    register repositories in modules

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
+import { Expense } from './entities/expense.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Expense])],
+  exports: [TypeOrmModule],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
