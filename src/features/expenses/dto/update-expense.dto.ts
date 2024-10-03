1e8d342874fe70d0c64e456ad4ce1d3ebad298a9
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-03 15:14:37 +0100

    Update update-expense.dto.ts

--- src/features/expenses/dto/update-expense.dto.ts
+++ src/features/expenses/dto/update-expense.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExpenseDto } from './create-expense.dto';
 
-export class UpdateExpenseDto extends PartialType(CreateExpenseDto) {}
+export class UpdateExpenseDto extends PartialType(CreateExpenseDto, {
+  skipNullProperties: true,
+}) {}
