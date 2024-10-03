ce020bbff4257d31b81cff653df9734a65260258
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-03 10:12:47 +0100

    change entity data type

--- src/features/users/entities/user.entity.ts
+++ src/features/users/entities/user.entity.ts
@@ -5,7 +5,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 @Entity({ name: 'Users' })
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   firstName: string;
