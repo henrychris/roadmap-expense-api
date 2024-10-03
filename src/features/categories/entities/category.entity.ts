a14f79cf1c326f112483507a49a33039e1596371
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-03 14:24:14 +0100

    change type of category id to string

--- src/features/categories/entities/category.entity.ts
+++ src/features/categories/entities/category.entity.ts
@@ -4,7 +4,7 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 @Entity({ name: 'Categories' })
 export class Category extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   name: string;
